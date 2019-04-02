9a517a4c4210f912f8d49ab98ffc596332fc3635
Eddie Chu <contact@eddiechu.me>
2019-04-01 20:54:25 -0700

    initial server setup

---
+++ server.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(express.json());
+app.use(
+    cors({
+        origin: ['http://localhost:3000']
+    })
+);
+
+app.get('/', (req, res) => {
+    res.json('hello');
+});
+
+app.listen(process.env.PORT || 8080, () => {
+    console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
+});
