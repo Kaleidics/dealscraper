29c5851f7f047265bd32ea3d6c5434f3e9868849
Eddie Chu <contact@eddiechu.me>
2019-04-01 21:25:13 -0700

    setup run,close server for db and potential tests

--- server.js
+++ server.js
@@ -15,6 +15,43 @@ app.get('/', (req, res) => {
     res.json('hello there');
 });
 
-app.listen(process.env.PORT || 8080, () => {
-    console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
-});
+let server;
+
+function runServer() {
+    return new Promise((resolve, reject) => {
+        mongoose.connect(DATABASE_URL, { useMongoClient: true }, err => {
+            if (err) {
+                return reject(err);
+            }
+            server = app
+                .listen(PORT, () => {
+                    console.log(`Your app is listening on port ${PORT}`);
+                    resolve();
+                })
+                .on('error', err => {
+                    mongoose.disconnect();
+                    reject(err);
+                });
+        });
+    });
+}
+
+function closeServer() {
+    return mongoose.disconnect().then(() => {
+        return new Promise((resolve, reject) => {
+            console.log('Closing server');
+            server.close(err => {
+                if (err) {
+                    return reject(err);
+                }
+                resolve();
+            });
+        });
+    });
+}
+
+if (require.main === module) {
+    runServer().catch(err => console.error(err));
+}
+
+module.exports = { app, runServer, closeServer };
